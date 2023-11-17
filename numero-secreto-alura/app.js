675306092a20771dcd3c849c737116785f43638c
Gabriel Ribeiro <gabrielribeiro.dev23@gmail.com>
2023-11-17 16:00:24 -0300

    Adicionado o projeto do número secreto ao repositório. Em desenvolvimento por Gabriel Ribeiro.

---
+++ numero-secreto-alura/app.js
@@ -0,0 +1,7 @@
+alert ("Olá, seja bem-vimdo(A) ao jogo Número Secreto!");
+let numeroSecreto = 23;
+let numeroChute = prompt ("Escolha um número entre 1 e 30");
+
+if (numeroChute == numeroSecreto) {
+    console.log ("Parabéns! Você acertou o número secreto! (23)");
+}
