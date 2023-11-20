b9354d429749c5d470f6dab626a5a6745ffb7b60
Gabriel Ribeiro <gabrielribeiro.dev23@gmail.com>
2023-11-20 16:22:38 -0300

    Atualizando o jogo número secreto, um desafio da alura!

--- numero-secreto-alura/app.js
+++ numero-secreto-alura/app.js
@@ -1,7 +1,14 @@
 alert ("Olá, seja bem-vimdo(A) ao jogo Número Secreto!");
-let numeroSecreto = 23;
+let numeroSecreto = 17;
+console.log(numeroSecreto)
 let numeroChute = prompt ("Escolha um número entre 1 e 30");
-
-if (numeroChute == numeroSecreto) {
-    console.log ("Parabéns! Você acertou o número secreto! (23)");
+// comparando um chute com o núnero secreto.
+if (numeroChute ==numeroSecreto) {
+    alert("Parabéns! Você ganhou o jogo do Número Secreto");
+}
+else if (numeroChute >numeroSecreto) {
+    alert(`O número secreto é menor que o ${numeroChute}`);
+}
+else {
+    alert(`O número secreto é maior que o ${numeroChute}`);
 }
