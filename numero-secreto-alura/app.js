ca68014ef6ff1a89e9007e3413a195dea11a2d72
Gabriel Ribeiro <gabrielribeiro.dev23@gmail.com>
2023-11-21 16:02:37 -0300

    O jogo do Número Secreto foi atualizado! Agora o jogo não termina ao errar-mos o número. Além disso, existe um contador de tentativas que irá verificar sempre a quantidade de tentativas feitas até acertar o número escondido.

--- numero-secreto-alura/app.js
+++ numero-secreto-alura/app.js
@@ -1,14 +1,22 @@
-alert ("Olá, seja bem-vimdo(A) ao jogo Número Secreto!");
-let numeroSecreto = 17;
-console.log(numeroSecreto)
-let numeroChute = prompt ("Escolha um número entre 1 e 30");
-// comparando um chute com o núnero secreto.
-if (numeroChute ==numeroSecreto) {
-    alert("Parabéns! Você ganhou o jogo do Número Secreto");
-}
-else if (numeroChute >numeroSecreto) {
-    alert(`O número secreto é menor que o ${numeroChute}`);
-}
-else {
-    alert(`O número secreto é maior que o ${numeroChute}`);
-}
+    //mensagem de boas vindas
+    alert ("Olá, seja bem-vimdo(A) ao jogo Número Secreto!");
+    // declarando variáveis
+    let numeroSecreto = 17;
+    console.log(numeroSecreto)
+    let numeroChute;
+    let tentativas = 1;
+    // Bloco de repetições.
+    while (numeroChute!=numeroSecreto){
+        numeroChute = prompt("Escolha um número de 1 a 30");
+        // comparando um chute com o núnero secreto e imprimindo o resultado na tela.
+        if (numeroChute ==numeroSecreto) {
+            alert(`Parabéns! Você ganhou o jogo do Número Secreto! com ${tentativas} tentativas`);
+        }
+        else if (numeroChute >numeroSecreto) {
+            alert(`O número secreto é menor que o ${numeroChute}`);
+        }
+        else {
+            alert(`O número secreto é maior que o ${numeroChute}`);
+        }
+        tentativas++;
+    }
